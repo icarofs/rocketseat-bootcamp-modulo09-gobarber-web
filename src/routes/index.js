364fc9e83d1aaaaf3358b629674105e00d85d805
Ícaro Ferreira de Souza <icaro.souza@ntendencia.com.br>
2020-06-21 18:12:36 -0300

    Config rotas privadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import RouteWrapper from './Route';
+
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
@@ -8,10 +10,10 @@ import Profile from '../pages/Profile';
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={SignIn} />
-      <Route path="/register" component={SignUp} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
+      <RouteWrapper path="/" exact component={SignIn} />
+      <RouteWrapper path="/register" component={SignUp} />
+      <RouteWrapper path="/dashboard" component={Dashboard} isPrivate />
+      <RouteWrapper path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 }
