8383d0d31cb872d82e924c077d9f30a4e4f96e9d
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-08-05 14:30:59 -0300

    Desenv funcionalidade de logout(action, reducer, sagas)

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -5,6 +5,7 @@ import { Form, Input } from '@rocketseat/unform';
 import { Container } from './styles';
 import { updateProfileRequest } from '../../store/modules/user/action';
 import AvatarInput from './AvatarInput';
+import { signOut } from '../../store/modules/auth/actions';
 
 export default function Profile() {
   const profile = useSelector((state) => state.user.profile);
@@ -12,6 +13,10 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
   }
 
   return (
@@ -39,7 +44,9 @@ export default function Profile() {
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
     </Container>
   );
 }
