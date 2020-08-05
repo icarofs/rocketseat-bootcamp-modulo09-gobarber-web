04e41064f085e52ca6a10075c3851eccf44736e9
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-08-05 12:08:40 -0300

    Adicionado foto de perfil na tela de profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,6 +4,7 @@ import { Form, Input } from '@rocketseat/unform';
 
 import { Container } from './styles';
 import { updateProfileRequest } from '../../store/modules/user/action';
+import AvatarInput from './AvatarInput';
 
 export default function Profile() {
   const profile = useSelector((state) => state.user.profile);
@@ -16,6 +17,8 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
 
