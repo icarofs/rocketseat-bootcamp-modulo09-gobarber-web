f07efa2ff618e41beeaf50340427f4224a330bca
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-08-05 10:54:50 -0300

    Desenvolvido edição de profile, (action, reducer, sagas)

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
 import { Container } from './styles';
+import { updateProfileRequest } from '../../store/modules/user/action';
 
 export default function Profile() {
   const profile = useSelector((state) => state.user.profile);
+  const dispatch = useDispatch();
 
   function handleSubmit(data) {
-    console.tron.log(data);
+    dispatch(updateProfileRequest(data));
   }
 
   return (
